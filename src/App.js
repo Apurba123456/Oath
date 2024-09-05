27db7c7d7bd0fbc5744b16e8d8f1a321c69e0704
Apurba123456 <apurbawin8@gmail.com>
2024-09-05 22:25:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Post } from "./pages/Post";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
